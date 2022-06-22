8604e1fdf00c5d4b3e8ee065aeb62221e6735c0f
DharmikVedic <dharmik.rathod@vedicrishiastro.com>
2022-06-22 21:07:04 +0530

    whatsapp

--- pages/plant-ganesha/product.js
+++ pages/plant-ganesha/product.js
@@ -251,7 +251,7 @@ export default function PlantGaneshaSlug() {
                           <a
                             target="_blank"
                             rel="noreferrer"
-                            href={`https://api.whatsapp.com/send?phone=919137606816`}
+                            href={`https://api.whatsapp.com/send?phone=918169882692`}
                             className="text-base md:mx-12 rounded-full hover:scale-[1.05] transform transition duration-200 ease-in bg-green-600 uppercase text-white font-semibold hover:bg-green-700 flex gap-3 justify-center items-center  px-4 py-3 max-w-sm w-full"
                           >
                             <span>
