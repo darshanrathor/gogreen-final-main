463334ef553261fd1f78589b912d627835c8481d
DharmikVedic <dharmik.rathod@vedicrishiastro.com>
2022-06-28 15:13:57 +0530

    add gtm

--- pages/_document.js
+++ pages/_document.js
@@ -4,17 +4,27 @@ class MyDocument extends Document {
         return (
             <Html>
                 <Head>
-            
-                    <link rel="preconnect" href="https://fonts.googleapis.com"/>
-                    <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin/>
+
+                    <link rel="preconnect" href="https://fonts.googleapis.com" />
+                    <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
                     <link href="https://fonts.googleapis.com/css2?family=Source+Sans+Pro:wght@300;400;600;700;900&display=swap"
-                          rel="stylesheet"/>
-                
+                        rel="stylesheet" />
+                    <script
+                        defer
+                        dangerouslySetInnerHTML={{
+                            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+      new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+      j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+      'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+      })(window,document,'script','dataLayer','GTM-NVNSL65');`,
+                        }}
+                    />
                 </Head>
                 <body>
-                <Main/>
-                <NextScript />
-
+                    <Main />
+                    <NextScript />
+                    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-NVNSL65"
+                        height="0" width="0" style={{ display: "none", visibility: "hidden" }} /></noscript>
                 </body>
             </Html>
         )
